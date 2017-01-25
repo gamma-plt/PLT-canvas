34c65e1562adaf09f45a542809e39b8120faecfe
scvalencia <sc.valencia606@uniandes.edu.co>
2017-01-25 11:13:11 -0500

    An interpreter for long integers

---
+++ pascal-interpreter/part01/java/exercises/ex03/Interpreter.java
@@ -0,0 +1,106 @@
+public class Interpreter {
+
+	// The expression to parse and calculate
+	String expression;
+
+	// Position in the expression
+	int position;
+
+	// Current Token being processed
+	Token currentToken;
+
+	public Interpreter(String expression) {
+		this.expression = expression;
+		this.position = 0;
+		this.currentToken = null;
+	}
+
+	private void error() {
+		System.out.println("Error parsing input expression");
+		System.exit(1);
+	}
+
+	// The scanner
+	private Token getNextToken() {
+
+		Token nextToken = null;
+
+		// End of Input
+		if(this.position >= this.expression.length())
+			nextToken = new Token(TokenType.EOF, null);
+
+		else {
+			/**
+			* Get a character at the position this.position and decide
+            * what token to create based on the single character
+			*/
+			char currentChar = this.expression.charAt(this.position);
+
+			/**
+			* If the character is a digit then converit to an integer, and
+			* return an INTEGER token. Increment this.position.
+			*/
+
+			if(Character.isDigit(currentChar)) {
+				nextToken = new Token(TokenType.INTEGER, currentChar - '0' + "");
+				this.position++;
+			} 
+
+			/**
+			* Otherwise, it is the operator. Create the token and 
+			* Increment this.position.
+			*/
+
+			else if(currentChar == '-') {
+				nextToken = new Token(TokenType.MINUS, currentChar + "");
+				this.position++;
+			} else
+				this.error();
+		}
+
+		currentToken = nextToken;
+		return nextToken;
+
+	}
+
+	/**
+	* Compare the current token type with the passed token type and if they 
+	* match then accept the current token and assign the next token to the 
+	* this.currentToken token. Otherwise, report an error.
+	*/
+	private void accept(TokenType expectedTokenType) {
+		if(this.currentToken.tokenType == expectedTokenType)
+			this.currentToken = this.getNextToken();
+		else
+			this.error();
+	}
+
+	// The parser and AST interpreter
+	// expr -> INTEGER PLUS INTEGER
+	public int interpret() {
+
+		// Initialize the variable with the first token
+		this.currentToken = this.getNextToken();
+
+		// According to the grammar, we're looking for a digit
+		Token left = this.currentToken;
+		this.accept(TokenType.INTEGER);
+
+		// According to the grammar, we're looking for a PLUS token
+		Token operator = this.currentToken;
+		this.accept(TokenType.MINUS);
+
+		// According to the grammar, we're looking for a digit
+		Token right = this.currentToken;
+		this.accept(TokenType.INTEGER);
+
+		// Fetch the data from both tokens, convert them to real integers
+		int leftValue = Integer.valueOf(left.value);
+		int rightValue = Integer.valueOf(right.value);
+
+		// Return the sum of those values
+		return leftValue - rightValue;
+
+	}
+
+}
