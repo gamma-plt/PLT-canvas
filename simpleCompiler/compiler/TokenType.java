72dac606ad32dca2e5bd49dbea96332eea2d3dc7
scvalencia <sc.valencia606@uniandes.edu.co>
2017-01-27 10:02:45 -0500

    Adding documentation to the TokenType class enum

--- simpleCompiler/compiler/TokenType.java
+++ simpleCompiler/compiler/TokenType.java
@@ -1,5 +1,43 @@
 /**
- * Created by scvalencia606 on 1/20/17.
+ * TokenType is an enum with constants for the types or categories
+ * that a Token object may have. Those categories are exclusive,
+ * and are used to identify the type for the compiler of a character
+ * stream. This is what distinguishes a token from another token.
+ * This is also the main input for parsing. The categories are:
+ *
+ * <li> {@link #EOI} </li>
+ * <li> {@link #MULTIPLICATION} </li>
+ * <li> {@link #DIVISION} </li>
+ * <li> {@link #MODULO} </li>
+ * <li> {@link #ADDITION} </li>
+ * <li> {@link #SUBTRACTION} </li>
+ * <li> {@link #NEGATE} </li>
+ * <li> {@link #EQUAL} </li>
+ * <li> {@link #NOT} </li>
+ * <li> {@link #NOT_EQUAL} </li>
+ * <li> {@link #LESS} </li>
+ * <li> {@link #LESS_EQUAL} </li>
+ * <li> {@link #GREATER} </li>
+ * <li> {@link #GREATER_EQUAL} </li>
+ * <li> {@link #ASSIGN} </li>
+ * <li> {@link #AND} </li>
+ * <li> {@link #OR} </li>
+ * <li> {@link #IF} </li>
+ * <li> {@link #ELSE} </li>
+ * <li> {@link #WHILE} </li>
+ * <li> {@link #PRINT} </li>
+ * <li> {@link #PUTC} </li>
+ * <li> {@link #LEFT_PARENTHESIS} </li>
+ * <li> {@link #RIGHT_PARENTHESIS} </li>
+ * <li> {@link #LEFT_CURLY} </li>
+ * <li> {@link #RIGHT_CURLY} </li>
+ * <li> {@link #SEMICOLON} </li>
+ * <li> {@link #COMMA} </li>
+ * <li> {@link #IDENTIFIER} </li>
+ * <li> {@link #INTEGER} </li>
+ * <li> {@link #STRING} </li>
+ *
+ * @author      Sebastián Valencia (sc.valencia606@gmail.com)
  */
 public enum TokenType {
 
@@ -10,7 +48,7 @@ public enum TokenType {
 	MULTIPLICATION, DIVISION, MODULO, ADDITION, SUBTRACTION,
 
     /* Relational operators */
-    NEGATE, NOT, LESS, LESS_EQUAL, GREATER, GREATER_EQUAL, EQUAL, NOT_EQUAL, ASSIGN,
+    NEGATE, EQUAL, NOT, NOT_EQUAL, LESS, LESS_EQUAL, GREATER, GREATER_EQUAL, ASSIGN,
 
 	/* Boolean operators */
 	AND, OR,
